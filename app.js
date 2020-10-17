e2803dbf2c1983cc0dd939167d25359eb9139bad
saikat-samanta <saikatsamanta737@gmail.com>
2020-10-17 23:37:08 +0530

    My first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const contentArr = ['Developer','Designer','Freelancer'];
+let textString='';
+let flag =false;
+let index =0;
+let speed = 400;
+
+let i = 0;
+const myTimer= () => {
+    if(flag){
+        textString = contentArr[index].substring(0,i);
+        document.querySelector('.type').textContent = textString;
+        i--;
+        if(textString ===''){
+            flag = false;
+            speed *=2;
+            if (index>=0 && index<2){
+                index++;
+            }
+            else if (index === 2){
+                index=0;
+            }
+        }
+    }else
+    {
+    textString = contentArr[index].substring(0,i);
+    document.querySelector('.type').textContent = textString;
+    i++;
+    if(textString ===contentArr[index]){
+        flag = true;
+        speed /= 2;
+    }
+    }
+}
+
+var myVar = setInterval(myTimer, speed);
